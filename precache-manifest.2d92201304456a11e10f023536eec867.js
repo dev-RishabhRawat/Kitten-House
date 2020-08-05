29b5e12c1cdec48ad0481d5018a96b281e076941
dev-RishabhRawat <dev.rishabh.2901@gmail.com>
2020-08-05 11:05:20 +0530

    Updates

---
+++ precache-manifest.2d92201304456a11e10f023536eec867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88028102aebecb082402ffcdc9cf25bb",
+    "url": "/dev-RishabhRawat/kitten-house/index.html"
+  },
+  {
+    "revision": "2bed8fce6bbc0df230f6",
+    "url": "/dev-RishabhRawat/kitten-house/static/css/main.742e3d05.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/dev-RishabhRawat/kitten-house/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dev-RishabhRawat/kitten-house/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bed8fce6bbc0df230f6",
+    "url": "/dev-RishabhRawat/kitten-house/static/js/main.34719a8d.chunk.js"
+  },
+  {
+    "revision": "d6c3151e25dc34758123",
+    "url": "/dev-RishabhRawat/kitten-house/static/js/runtime-main.1da33c3d.js"
+  }
+]);
